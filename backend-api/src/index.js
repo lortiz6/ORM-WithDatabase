364ee0b44edf0e5acb9d716216a2b9a09bdf4995
Liliana Ortiz <lilianadcortiz@gmail.com>
2024-03-27 16:27:50 -0500

    added index.js for prisma CRUD operations for the backend

---
+++ backend-api/src/index.js
@@ -0,0 +1,51 @@
+// src/index.js
+
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = gql`
+  type Query {
+    customers: [Customer!]!
+  }
+
+  type Customer {
+    id: Int!
+    name: String!
+    email: String!
+  }
+
+  type Mutation {
+    createCustomer(name: String!, email: String!): Customer!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    customers: async () => {
+      return await prisma.customer.findMany();
+    },
+  },
+  Mutation: {
+    createCustomer: async (_, { name, email }) => {
+      return await prisma.customer.create({
+        data: {
+          name,
+          email,
+        },
+      });
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
