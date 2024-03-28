4bc9cdf75d7996ec639e66a576d3363147d330f2
Liliana Ortiz <lilianadcortiz@gmail.com>
2024-03-27 23:49:04 -0500

    updated files

---
+++ frontend-web-app/src/app.js
@@ -0,0 +1,21 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import CustomerList from './components/CustomerList';
+
+//GraphQL endpoint
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>ORM - Customer Management App</h1>
+        <CustomerList />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
