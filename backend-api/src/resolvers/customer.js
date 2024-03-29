41e27ab618a0cab4d70415015f5f0a792f0500fa
Liliana Ortiz <lilianadcortiz@gmail.com>
2024-03-29 01:18:19 -0500

    updated customer.js in my resolver folder

--- backend-api/src/resolvers/customer.js
+++ backend-api/src/resolvers/customer.js
@@ -1,19 +1,18 @@
 const customerModel = require('../models/customer');
+
 const customerResolvers = {
   Query: {
-
     customers: async () => {
       return await customerModel.getAllCustomers();
     },
   },
   Mutation: {
-
-    createCustomer: async (_, { name, email }) => {
-      return await customerModel.createCustomer(name, email);
+    createCustomer: async (_, { name, email, description }) => {
+      return await customerModel.createCustomer(name, email, description);
     },
 
-    updateCustomer: async (_, { id, name, email }) => {
-      return await customerModel.updateCustomer(id, name, email);
+    updateCustomer: async (_, { id, name, email, description, orders }) => {
+      return await customerModel.updateCustomer(id, name, email, description, orders);
     },
 
     deleteCustomer: async (_, { id }) => {
