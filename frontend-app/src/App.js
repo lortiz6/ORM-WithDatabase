c78e408cd529eb8d5edefadd4440778bdcb4d9bd
Liliana Ortiz <lilianadcortiz@gmail.com>
2024-04-29 23:16:48 -0500

    updated App.js

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -5,6 +5,7 @@ import CustomersPage from './pages/CustomersPage';
 import OrdersPage from './pages/OrdersPage';
 import ProductsPage from './pages/ProductsPage';
 import SearchBar from './components/SearchBar';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './css/style.css';
 
 function App() {
@@ -15,30 +16,37 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className="app-container">
-        <header className="topbar">
-          <nav className="navbar">
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/customers">Customers</a></li>
-              <li><a href="/orders">Orders</a></li>
-              <li><a href="/products">Products</a></li>
-            </ul>
-            <SearchBar handleSearch={handleSearch} /> {}
-          </nav>
-        </header>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/customers" element={<CustomersPage searchQuery={searchQuery} />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/products" element={<ProductsPage />} />
-          </Routes>
+    <ApolloProvider client={client}> {}
+      <Router>
+        <div className="app-container">
+          <header className="topbar">
+            <nav className="navbar">
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/customers">Customers</a></li>
+                <li><a href="/orders">Orders</a></li>
+                <li><a href="/products">Products</a></li>
+              </ul>
+              <SearchBar handleSearch={handleSearch} /> {}
+            </nav>
+          </header>
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/customers" element={<CustomersPage searchQuery={searchQuery} />} />
+              <Route path="/orders" element={<OrdersPage />} />
+              <Route path="/products" element={<ProductsPage />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 }
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default App;
